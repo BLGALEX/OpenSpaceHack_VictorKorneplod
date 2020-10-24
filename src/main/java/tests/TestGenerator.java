d2d9454fbade6ca3b2091f3e14b37a092c994999
ephemeralsad <sarancev.dev@gmail.com>
2020-10-24 18:15:30 +1000

    Removed mistake with TestGenerator

--- src/main/java/tests/TestGenerator.java
+++ src/main/java/tests/TestGenerator.java
@@ -1,20 +1,5 @@
 package tests;
 
-
-enum RB {
-    INTERNET,
-    MOBILE
-}
-
-class Record {
-    private String request;
-    private String clarification;
-    private RB rb;
-    private String question;
-    public int nSteps;
-    public int[] steps;
-}
-
 public class TestGenerator {
 
 }
