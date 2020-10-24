a00d4a5fb744a0de9e5f18cf2072a7bfc96888e3
ephemeralsad <sarancev.dev@gmail.com>
2020-10-24 18:53:48 +1000

    Created Test & TestGenerator

---
+++ src/main/java/tests/Test.java
@@ -0,0 +1,37 @@
+package tests;
+
+public class Test {
+    private int classification;
+    private String input;
+    private int output;
+
+    Test(int classification, String input, int output) {
+        this.classification = classification;
+        this.input = input;
+        this.output = output;
+    }
+
+    public int getClassification() {
+        return classification;
+    }
+
+    public void setClassification(int classification) {
+        this.classification = classification;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public int getOutput() {
+        return output;
+    }
+
+    public void setOutput(int output) {
+        this.output = output;
+    }
+}
