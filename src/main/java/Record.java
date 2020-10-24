3f281743e1991c88bb94f245b89d593335b035a6
ephemeralsad <sarancev.dev@gmail.com>
2020-10-24 18:17:22 +1000

    Record Correction

--- src/main/java/Record.java
+++ src/main/java/Record.java
@@ -8,6 +8,45 @@ public class Record {
     private String clarification;
     private RB rb;
     private String question;
-    public int nSteps;
-    public int[] steps;
+    private int[] steps;
+
+    public String getRequest() {
+        return request;
+    }
+
+    public String getClarification() {
+        return clarification;
+    }
+
+    public RB getRb() {
+        return rb;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setRequest(String request) {
+        this.request = request;
+    }
+
+    public void setClarification(String clarification) {
+        this.clarification = clarification;
+    }
+
+    public void setRb(RB rb) {
+        this.rb = rb;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public int[] getSteps() {
+        return steps;
+    }
+
+    public void setSteps(int[] steps) {
+        this.steps = steps;
+    }
 }
