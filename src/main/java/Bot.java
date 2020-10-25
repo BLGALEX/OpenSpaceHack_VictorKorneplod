5aaf7b56a12e1a18b35d6dfcd2578962f6ba2e5e
BLGALEX <lepehaleha@bk.ru>
2020-10-25 15:03:50 +1000

    README changed

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
+
     public static void main(String[] args) {
         ApiContextInitializer.init();
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
